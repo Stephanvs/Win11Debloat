72fc4242a64bc87fbb20bcaf07b0689c96ae1ccc
Stephan van Stekelenburg <Stephanvs@users.noreply.github.com>
2024-07-11 10:24:04 +0000

    chore: update script location

--- Get.ps1
+++ Get.ps1
@@ -56,7 +56,7 @@ Write-Output "------------------------------------------------------------------
 Write-Output "> Downloading Win11Debloat..."
 
 # Download latest version of Win11Debloat from github as zip archive
-Invoke-WebRequest http://github.com/raphire/win11debloat/archive/master.zip -OutFile "$env:TEMP/win11debloat-temp.zip"
+Invoke-WebRequest http://github.com/stephanvs/win11debloat/archive/master.zip -OutFile "$env:TEMP/win11debloat-temp.zip"
 
 # Remove old script folder if it exists
 if(Test-Path "$env:TEMP/Win11Debloat") {
